91a01450c499e7c8bf49197093ffeb454b734c20
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2023-03-23 08:43:45 +0100

    responsive header

--- biblio/src/components/Header.js
+++ biblio/src/components/Header.js
@@ -6,7 +6,7 @@ import { React } from "react";
 function Header({ searchTerm, setSearchTerm }) {
 
   return (
-    <div className="Header flex items-center w-100">
+    <div className="Header flex items-center">
         <div className="flex flex-row items-center">
             <img src={logo} alt="logo" className="w-10 mb-1.5"/>
             <h1 className="text ml-5 text-2xl mb-0 font-700">IUT - Bibliothèque</h1>
