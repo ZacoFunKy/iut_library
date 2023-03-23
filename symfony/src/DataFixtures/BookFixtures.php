148ea602fda564af28241ddddda08d1080943b99
ztanji <zachary.tanji@etu.u-bordeaux.fr>
2023-03-23 11:09:37 +0100

    Books Data

--- symfony/src/DataFixtures/BookFixtures.php
+++ symfony/src/DataFixtures/BookFixtures.php
@@ -16,7 +16,7 @@ class BookFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // By using Google Books API get 200 random books and add them to the database
+        /*
         for($i = 0; $i < 200; $i=$i+40) {
             $base_url= "https://www.googleapis.com/books/v1/volumes?q=inauthor&maxResults=40&startIndex=$i";
             $json = file_get_contents($base_url);
@@ -35,8 +35,7 @@ class BookFixtures extends Fixture
                 $livre->setDateparution($dateParuption);
                 $livre->setPage($results['volumeInfo']['pageCount']);
                 $livre->setCouverture($results['volumeInfo']['imageLinks']['thumbnail']);
-                if(!isset($results['volumeInfo']['publisher'])) {
-                } else {
+                if(isset($results['volumeInfo']['publisher'])) {
                     $editor = $results['volumeInfo']['publisher'];
                     $editeur = $manager->getRepository(Editeur::class)->findOneBy(['nomediteur' => $editor]);
                     if($editeur == null) {
@@ -46,31 +45,44 @@ class BookFixtures extends Fixture
                     }
                     $livre->setIdEditeur($editeur);
                 }
-                if(!isset($results['volumeInfo']['categories'])){
-                } else {
+                if(isset($results['volumeInfo']['categories'])){
                     $categoris = $results['volumeInfo']['categories'];
-                }
-                foreach($categoris as $categoris) {
-                    $genre = $manager->getRepository(Categorie::class)->findOneBy(['nomgenre' => $categoris]);
-                    if($genre == null) {
-                        $genre = new Categorie();
-                        $genre->setNomcategorie($categoris);
-                        $manager->persist($genre);
+                    foreach($categoris as $categoris) {
+                        $genre = $manager->getRepository(Categorie::class)->findOneBy(['nomgenre' => $categoris]);
+                        if($genre == null) {
+                            $genre = new Categorie();
+                            $genre->setNomcategorie($categoris);
+                            $manager->persist($genre);
+                        }
+                        $livre->addNomcategorie($genre);
                     }
-                    $livre->addNomcategorie($genre);
                 }
-                $langue = $results['volumeInfo']['language'];
-                $langue = $manager->getRepository(Langue::class)->findOneBy(['nomlangue' => $langue]);
-                if($langue == null) {
-                    $langage = new Langue();
-                    $langage->setNomlangue($langue);
-                    $manager->persist($langue);
+                if(isset($results['volumeInfo']['language'])){
+                    $langue = $results['volumeInfo']['language'];
+                    $langue = $manager->getRepository(Langue::class)->findOneBy(['nomlangue' => $langue]);
+                    if($langue == null) {
+                        $language = new Langue();
+                        $language->setNomlangue($langue);
+                        $manager->persist($language);
+                    }
+                }
+                if(isset($results['volumeInfo']['authors'])) {
+                    $authors = $results['volumeInfo']['authors'];
+                    foreach($authors as $authors) {
+                        $auteur = $manager->getRepository(Auteur::class)->findOneBy(['nomauteur' => $authors]);
+                        if($auteur == null) {
+                            $auteur = new Auteur();
+                            $auteur->getIntuituleauteur($authors);
+                            $manager->persist($auteur);
+                        }
+                        $livre->addIdAuteur($auteur);
+                    }
                 }
                 $manager->persist($livre);
             }
         }
 
-        $manager->flush();
+        $manager->flush();*/
 
     }
 }
